487b75100577195f34fc7a292247935eea1064d8
stefanolazzari <stefanolazzari79@libero.it>
2025-03-13 13:07:44 +0000

    configurazione express

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-console.log("Applicazione partita");
+const express = require("express"); 
+const app = express();
+
+const statici = express.static('public');
+app.use(statici);
+
+app.get('/', (req,res)=>{
+    res.send("ciao");
+})
+
+app.listen(3000,()=> console.log("in ascolto sulla porta 3000"));
+
+
+
+
 
